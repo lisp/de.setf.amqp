36df8ea469f3ccd8db5953f5a1eecc045364bbfd
james anderson <james.anderson@setf.de>
2010-02-17 12:41:43 +0100

    bring 0.8r0 and 0.9r1 codec tests up-to-date

--- test/AMQP-1-1-0-8-0/test.lisp
+++ test/AMQP-1-1-0-8-0/test.lisp
@@ -1,8 +1,20 @@
 ;;; -*- Package: de.setf.amqp.implementation; -*-
 
-;;; test amqp-1-1-0-8-0 transport operators
-
 (in-package :de.setf.amqp.implementation)
+
+(document :file
+ (description "This file defines codec tests for 0.8r0 components of the 'de.setf.amqp' library.")
+ (copyright
+  "Copyright 2010 [james anderson](mailto:james.anderson@setf.de)"
+  "'de.setf.amqp' is free software: you can redistribute it and/or modify it under the terms of version 3
+  of the GNU Affero General Public License as published by the Free Software Foundation.
+
+  'setf.amqp' is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License along with 'de.setf.amqp'.
+  If not, see the GNU [site](http://www.gnu.org/licenses/)."))
 
 ;;;
 ;;; test elementary class/method instantiation, classes w/ initargs,
