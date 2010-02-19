72eef6ddb0baba7bd2fd92b427155c5475ebda13
james anderson <james.anderson@setf.de>
2010-02-19 12:52:52 +0100

    add 0.9r0 connection negotiation for qpid

---
+++ AMQP-1-1-0-9-0/abstract-classes.lisp
@@ -0,0 +1,42 @@
+;;; -*- Package: de.setf.amqp.implementation; -*-
+
+(in-package :de.setf.amqp.implementation)
+
+
+(document :file
+  (description "This file defines abstract protocol classes for AMPQ version 0.9r0 components of the
+ `de.setf.amqp` library.")
+ (copyright
+  "Copyright 2010 [james anderson](mailto:james.anderson@setf.de) All Rights Reserved"
+  "'de.setf.amqp' is free software: you can redistribute it and/or modify it under the terms of version 3
+  of the GNU Affero General Public License as published by the Free Software Foundation.
+
+  'setf.amqp' is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the Affero General Public License for more details.
+
+  A copy of the GNU Affero General Public License should be included with 'de.setf.amqp' as `AMQP:agpl.txt`.
+  If not, see the GNU [site](http://www.gnu.org/licenses/)."))
+
+
+(defvar amqp-1-1-0-9-0::+protocol-version+
+  :amqp-1-1-0-9-0
+  "Specifies the protocol header for the highest supported version.")
+
+(setf (version-protocol-header amqp-1-1-0-9-0::+protocol-version+) #(65 77 81 80 1 1 0 9))
+
+
+(defclass amqp-1-1-0-9-0:object (amqp:object)
+  ((version
+   :initform '(1 1 0 9 0)
+   :allocation :class)))
+
+
+(defclass amqp-1-1-0-9-0:method (amqp:method)
+  ((version
+   :initform '(1 1 0 9 0)
+   :allocation :class)))
+
+
+(defmethod method-argument-offset ((method amqp-1-1-0-9-0::method))
+  4)
---
+++ AMQP-1-1-0-9-0/device-level.lisp
@@ -0,0 +1,59 @@
+;;; -*- Package: de.setf.amqp.implementation; -*-
+
+(in-package :de.setf.amqp.implementation)
+
+
+(document :file
+  (description "This file defines version-specific connecttion and channel operators for AMPQ version 0.9r0
+ of the `de.setf.amqp` library.")
+ (copyright
+  "Copyright 2010 [james anderson](mailto:james.anderson@setf.de) All Rights Reserved"
+  "'de.setf.amqp' is free software: you can redistribute it and/or modify it under the terms of version 3
+  of the GNU Affero General Public License as published by the Free Software Foundation.
+
+  'setf.amqp' is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the Affero General Public License for more details.
+
+  A copy of the GNU Affero General Public License should be included with 'de.setf.amqp' as `AMQP:agpl.txt`.
+  If not, see the GNU [site](http://www.gnu.org/licenses/)."))
+
+
+(defmethod shared-initialize ((instance amqp-1-1-0-9-0:connection) (slots t) &key)
+  ;; set circumspectly in order to allow update for changed class
+  (call-next-method)
+  (flet ((conditionally-set (name value)
+           (unless (slot-boundp instance name)
+             (setf (slot-value instance name) value))))
+    (conditionally-set 'input-frame-class '7-byte-header-input-frame)
+    (conditionally-set 'output-frame-class '7-byte-header-output-frame)))
+
+
+(defmethod connection-frame-type-class-name ((connection amqp-1-1-0-9-0:connection) code)
+  (ecase code
+    (0 nil)
+    (1 'amqp:method)
+    (2 'amqp:header)
+    (3 'amqp:body)
+    (4 'amqp:oob-method)
+    (5 'amqp:oob-header)
+    (6 'amqp:oob-body)
+    (7 'amqp:trace)
+    (8 'amqp:heartbeat)
+    (9 'amqp:request)
+    (10 'amqp:response)))
+
+(defmethod connection-class-name-frame-type ((connection amqp-1-1-0-9-0::connection) name)
+  (ecase name
+    ((nil) 0)
+    (amqp:method 1)
+    (amqp:header 2)
+    (amqp:body 3)
+    (amqp:oob-method 4)
+    (amqp:oob-header 5)
+    (amqp:oob-body 6)
+    (amqp:trace 7)
+    (amqp:heartbeat 8)
+    (amqp:request 9)
+    (amqp:response 10)))
+
